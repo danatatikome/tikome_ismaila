733252d24cde61fa93d1785c6648dc8d4923badf
danatatikome <adjiasaidou1@gmail.com>
2023-12-23 21:57:02 +0100

    ajout de Site_Perso_Tikome.html

---
+++ Notifier.js
@@ -0,0 +1,4 @@
+let Form = document.getElementById('form');
+Form.addEventListener('submit', () => {
+    alert("Votre message a bien été envoyé. Veuillez me contacter pour plus d'information!");
+}); 
